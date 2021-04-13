075fe766b4827bc5ba44d9e881821472203ff098
Sebastian Diaz <sebastian.diaz@segic.usach.cl>
2021-04-13 16:23:28 -0400

    Se implementa insert3

--- code/db-basico.php
+++ code/db-basico.php
@@ -47,8 +47,18 @@ $stmt = $db->prepare("SELECT * FROM users");
 $stmt->execute();
 $users= $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+echo '<table>
+    <tr>
+        <th> ID </th>
+        <th> Nombre</th>
+    </tr>';
+
 foreach($users as $user){
-    echo $user['id'] . "</br>";
-    echo $user['full_name']. "</br>";
+    echo'<tr>
+    <td>' . $user['id'] . '</td>
+    <td>' . $user['full_name']. '</td>
+    </tr>';
+
 }
+
 
