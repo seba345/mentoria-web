8cfbf96544eb3df2d7a1ff357ca5e1deecdbaf49
Sebastian Diaz <sebastian.diaz@segic.usach.cl>
2021-04-12 22:30:22 -0400

    Se implementa conexion db

--- code/db-basico.php
+++ code/db-basico.php
@@ -1,3 +1,8 @@
 <?php
 
-echo "Este es el archivo basico de DB";
+$dbname="registro";
+$dbuser="registro_user";
+$dbpassword="user2";
+
+$dsn ="mysql:host=localhost;dbname=$dbname";
+$db = new PDO($dsn,$dbuser, $dbpassword);
