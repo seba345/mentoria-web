cf035882dc335728abf8ffe3a8c7598249cdcec2
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-04-15 16:58:44 -0400

    cambio

--- code/db-basico.php
+++ code/db-basico.php
@@ -1,64 +1,120 @@
 <?php
 
-$dbname="registro";
-$dbuser="admin";
-$dbpassword="user1";
-
-
+$dbname = "registro";
+$dbuser = "admin";
+$dbpassword = "user1";
 
 try {
+    $dsn = "mysql:host=localhost;dbname=$dbname";
+    $db = new PDO($dsn, $dbuser, $dbpassword);
+    echo "Correcto";
 
-    $dsn ="mysql:host=localhost;dbname=$dbname";
-    $db = new PDO($dsn,$dbuser, $dbpassword);
-
-   echo "Insertó correctamente". "</br>";
-} catch (PDOException $e){
-        echo $e->getMessage();
+} catch (PDOException $e) {
+    echo $e->getMessage();
 }
 
+ /*   
+// Preparar la INSERT
+$sql ="INSERT INTO users
+(full_name, email, user_name, password)
+VALUES
+(:full_name, :email, :user_name, :password)";
+
+// stament
+$stmt = $db->prepare($sql);
+
+$full_name = 'Sebastian Diaz';
+$email = 'sebastian.diaz@segic.usach.cl';
+$user_name = "seba.diaz";
+$password = password_hash("user22", PASSWORD_DEFAULT);
+
+$stmt->bindParam(':full_name', $full_name);
+$stmt->bindParam(':email', $email);
+$stmt->bindParam(':user_name', $user_name);
+$stmt->bindParam(':password', $password);
+
+$stmt->execute();
+
+//password_verify("user4", "valor de la bd") //devuelve verdadero si coincide  
+*/
+
 /*
-        //preparar consulta
-        $sql = "INSERT INTO users 
-        (full_name,email,user_name, password)
-        values
-        (:full_name, :email, :user_name, :password)";
+// Preparar la INSERT Masivo
+$users = [
+            [
+                'name'=>'miguel', 
+                'email'=>'miguel.p@usach', 
+                'username'=>'miguel', 
+                'password'=>'miguel123'
+            ],
+            [
+                'name'=>'andrea', 
+                'email'=>'andrea.a@usach', 
+                'username'=>'andrea', 
+                'password'=>'andrea123'
+            ]
+        ];
+    
 
-        //statement
-        $stmt= $db->prepare($sql);
+$sql ="INSERT INTO users
+(full_name, email, user_name, password)
+VALUES
+(:full_name, :email, :user_name, :password)";
 
+// stament
+$stmt = $db->prepare($sql);
 
-        $full_name ='Juan Perez';
-        $email ='juan.perez@segic.cl';
-        $user_name = 'juan.perez';
-        $password = 'juan123';
+foreach ($users as $user){   
 
-        $stmt->bindParam(':full_name', $full_name);
-        $stmt->bindParam(':email', $email);
-        $stmt->bindParam(':user_name', $user_name);
-        $stmt->bindParam(':password', $password);
+    $stmt->bindParam(':full_name', $user['name']);
+    $stmt->bindParam(':email', $user['email']);
+    $stmt->bindParam(':user_name', $user['username']);
+    $password = password_hash($user['password'], PASSWORD_DEFAULT);
+    $stmt->bindParam(':password', $password);
+                               
+    $stmt->execute();
+}
+*/
 
-        $stmt->execute();
+/*
+// Preparar la DELETE
+$id = 2;
+$sql ="DELETE FROM users
+WHERE id = :id";
+
+// stament
+$stmt = $db->prepare($sql);
+// no se pueden pasar valores directos, solo referencias
+$stmt->bindParam(':id', $id);
+
+$stmt->execute();
 */
 
 
+// Preparar la SELECT
+$sql ="SELECT id, full_name, user_name, email
+       FROM users";
+// stament
+$stmt = $db->prepare($sql);
 
-//queryin data
-$stmt = $db->prepare("SELECT * FROM users");
 $stmt->execute();
-$users= $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo '<table>
-    <tr>
-        <th> ID </th>
-        <th> Nombre</th>
-    </tr>';
-
-foreach($users as $user){
-    echo'<tr>
-    <td>' . $user['id'] . '</td>
-    <td>' . $user['full_name']. '</td>
-    </tr>';
-
-}
 
 
+$users = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+
+echo '<table border="1">
+        <tr>
+            <th>ID</th>
+            <th>NOMBRE COMPLETO</th>
+            <th>USUARIO</th>
+            <th>CORREO</th>
+        </th>';
+            
+foreach ($users as $user) {
+    echo '<tr>
+            <th>' .$user['id']. '</th>
+            <th>' .$user['full_name']. '</th>
+            <th>' .$user['user_name']. '</th>
+            <th>' .$user['email']. '</th>
+          </th>';
+};
