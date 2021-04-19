e7bf4e9629fe154afb3368f84c4bbfd51584f44d
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-04-19 12:46:38 -0400

    primer commit

---
+++ code/util/db.php
@@ -0,0 +1,17 @@
+<?php
+
+function connectDB()
+{
+    $dbname = "registro";
+    $dbuser = "registro-user";
+    $dbpassword = "user1";
+
+    try {
+        $dsn = "mysql:host=localhost;dbname=$dbname";
+        $db = new PDO($dsn, $dbuser, $dbpassword);
+        return $db;
+
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
