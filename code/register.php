4f2e25faf129b5c5530228e271b39ab5dc0b1a4d
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-04-23 12:32:07 -0400

    fff

--- code/register.php
+++ code/register.php
@@ -1,7 +1,54 @@
+<?php
+// codigo PHP
+require "util/db.php";
+$db = connectDB();
+
+if (isset($_POST["sign-up-button"])){
+	print_r($_POST);
+
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$username = $_POST["username"];
+	$pass = $_POST["pass"];
+	$repeatPass = $_POST["repeat-pass"];
+	$rememberMe = $_POST["remember-me"];
+	
+$sql ="INSERT INTO users
+(full_name, email, user_name, password)
+VALUES
+(:full_name, :email, :user_name, :password)";
+
+// stament
+$stmt = $db->prepare($sql);
+
+//foreach ($users as $user){   
+
+    $stmt->bindParam(':full_name', $name);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':user_name', $username);
+    $password = password_hash($pass, PASSWORD_DEFAULT);
+    $stmt->bindParam(':password', $password);
+                               
+    $stmt->execute();
+
+	echo "Registro almacenado";
+//}
+
+
+}
+else{
+	echo "no se ha enviado la informacion";
+}
+
+$message ="registro Realizado con exito";
+$valido =0;
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Sign in</title>
+	<title>Registro Mentoria Web Raul Marin</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
@@ -43,16 +90,29 @@
 			<div class="login100-more" style="background-image: url('images/bg-01.jpg');"></div>
 
 			<div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
-				<form class="login100-form validate-form" method="POST" action="register.php">
+				<form class="login100-form validate-form" method="POST" action="process.php?id=finanzas&payment=100">
 				<input type="hidden" name="super-secreto" value="valor super secreto">
 
 					<span class="login100-form-title p-b-59">
 						Sign Up
 					</span>
 
+					<?php if ($valido == 1): ?>
+					<p class="msg-form"> Este es un texto controlado desde PHP </p>
+					<?php endif; ?>
+					
 
+					<div class="wrap-input100 validate-input" data-validate="Name is required">
+						<span class="label-input100">Full Name</span>
+						<input class="input100" type="text" name="name" placeholder="Name...">
+						<span class="focus-input100"></span>
+					</div>
 
-	
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<span class="label-input100">Email</span>
+						<input class="input100" type="text" name="email" placeholder="Email addess...">
+						<span class="focus-input100"></span>
+					</div>
 
 					<div class="wrap-input100 validate-input" data-validate="Username is required">
 						<span class="label-input100">Username</span>
@@ -63,6 +123,12 @@
 					<div class="wrap-input100 validate-input" data-validate = "Password is required">
 						<span class="label-input100">Password</span>
 						<input class="input100" type="text" name="pass" placeholder="*************">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Repeat Password is required">
+						<span class="label-input100">Repeat Password</span>
+						<input class="input100" type="text" name="repeat-pass" placeholder="*************">
 						<span class="focus-input100"></span>
 					</div>
 
@@ -85,13 +151,16 @@
 					<div class="container-login100-form-btn">
 						<div class="wrap-login100-form-btn">
 							<div class="login100-form-bgbtn"></div>
-							<button class="login100-form-btn" name="sign-in-button">
+							<button class="login100-form-btn" name="sign-up-button">
 								Sign Up
 							</button>
 							
 						</div>
 
-					
+						<a href="#" class="dis-block txt3 hov1 p-r-30 p-t-10 p-b-10 p-l-30">
+							Sign in
+							<i class="fa fa-long-arrow-right m-l-5"></i>
+						</a>
 					</div>
 				</form>
 			</div>
