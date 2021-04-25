9360a08cd0643ae2f4a865936ff7d5b6919ac332
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-04-25 18:52:07 -0400

    tareadd

--- code/index.php
+++ code/index.php
@@ -88,7 +88,7 @@ $users = $stmt -> fetchAll(PDO::FETCH_ASSOC);
                         <!-- <a href="view.php"><button class="btn btn-primary btn-sm" method="POST" action="view.php">View</button></a>
                         // <a href="edit.php"><button class="btn btn-outline-primary btn-sm">Edit</button></a> -->
                 
-                        <a href="view.php?id='<?= $user['full_name']?>'"><button class="btn btn-primary btn-sm" >View</button></a>
+                        <a href="view.php?full_name='<?= $user['full_name']?>'&id='<?= $user['id']?>'"><button class="btn btn-primary btn-sm" >View</button></a>
                        
                         <a href="edit.php"><button class="btn btn-outline-primary btn-sm">Edit</button></a>
                         <button class="btn btn-sm">Delete</button>
