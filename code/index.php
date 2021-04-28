15fc1fbcfa349365c606b4c7a42e418516143fec
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-04-28 12:55:00 -0400

    fffdj

--- code/index.php
+++ code/index.php
@@ -37,7 +37,7 @@ $users = $stmt -> fetchAll(PDO::FETCH_ASSOC);
     
     <div class="container pt-4 pb-4">
         <nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
-            <a class="navbar-brand" href="#">HTML CRUD Template</a>
+            <a class="navbar-brand" href="#">HTML CRUD</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -45,10 +45,10 @@ $users = $stmt -> fetchAll(PDO::FETCH_ASSOC);
             <div class="collapse navbar-collapse" id="navbarsExample09">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="create.php">Create</a>
+                        <a class="nav-link" href="create.php">Crear Usuarios</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">FAQ</a>
