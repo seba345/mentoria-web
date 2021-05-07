49e28675272c0653d7f0f4e29b846c8e4560d77a
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-05-07 10:17:52 -0400

    probandosd

--- code/index.php
+++ code/index.php
@@ -4,16 +4,11 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new \app\core\Aplication();
 
-//$router = new Router();
-
-//$router->get('/', function(){
-//    return "Hola Mundo";
-//});
-$app->$router->get('/', function(){
+$app->router->get('/', function(){
     return "Hola Mundo";
 });
 
-$app->$router->get('/contact', function(){
+$app->router->get('/contact', function(){
     return "Contact";
 });
 
