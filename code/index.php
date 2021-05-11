00f7add50a5cc0b1e8071b67e8afeea6168933fc
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-05-11 19:16:47 -0400

    framework

--- code/index.php
+++ code/index.php
@@ -4,12 +4,15 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new \app\core\Aplication();
 
-$app->router->get('/code/', function(){
-    return "Hola Mundo";
-});
 
 $app->router->get('/code/contacto', function(){
     return "Contact";
 });
+
+$app->router->get('/code/', function(){
+    return "Hola Mundo";
+});
+
+
 
 $app->run();
