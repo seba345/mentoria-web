08d0f4dee427b8aa2f5faf853089381ce74bd6b2
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-05-18 11:03:38 -0400

    se modifica

--- code/public/index.php
+++ code/public/index.php
@@ -5,9 +5,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new \app\core\Aplication();
 
 
-$app->router->get('/code/contacto', 'contact');
+$app->router->get('/code/public/contacto', 'contact');
  
-$app->router->get('/code/', function(){
+$app->router->get('/code/public/', function(){
     return "Hola Mundo";
 });
 
