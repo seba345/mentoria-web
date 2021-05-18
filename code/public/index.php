63b77cf4feec3d8d1925ce325f48a121d8ee3ced
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-05-18 10:21:49 -0400

    ordenando MVC

--- code/public/index.php
+++ code/public/index.php
@@ -8,11 +8,10 @@ $app = new \app\core\Aplication();
 $app->router->get('/code/contacto', function(){
     return "Contact";
 });
-
+ 
 $app->router->get('/code/', function(){
     return "Hola Mundo";
 });
-
 
 
 $app->run();
