c10bcfff4ac46766dcaaa2ddb25b30d0c66231cd
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-05-20 15:21:04 -0400

    se agrega natbar

--- code/public/index.php
+++ code/public/index.php
@@ -5,9 +5,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new \app\core\Aplication();
 
 
+//$app->router->get('/code/public/contact', 'contact');
+ 
+//$app->router->get('/code/public/', 'home');
+
 $app->router->get('/code/public/contact', 'contact');
  
-$app->router->get('/code/public/', 'home');
+$app->router->get('/code/public/', 'contact');
 
 
 $app->run();
