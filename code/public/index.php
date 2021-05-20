d19609f69f8f17f351ba9faa6f14a72e059df68d
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-05-20 16:19:15 -0400

    reemplaza contenido

--- code/public/index.php
+++ code/public/index.php
@@ -14,7 +14,7 @@ $app = new Aplication(dirname(__DIR__));
 
 $app->router->get('/code/public/contact', 'contact');
  
-$app->router->get('/code/public/', 'home');
+$app->router->get('/code/public/', 'contact');
 
 
 $app->run();
