e37ba4ced814facb7e91ccfe4e3d7ecef915c351
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-05-20 15:13:11 -0400

    modificando contact

--- code/public/index.php
+++ code/public/index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new \app\core\Aplication();
 
 
-$app->router->get('/code/public/contacto', 'contact');
+$app->router->get('/code/public/contact', 'contact');
  
 $app->router->get('/code/public/', 'home');
 
