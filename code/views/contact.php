a33df63216cda74760fc1499a57f7476975f1d9c
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-05-27 10:20:11 -0400

    se agrega formulario contacto

--- code/views/contact.php
+++ code/views/contact.php
@@ -1,17 +1,16 @@
 <h1>Contact1!</h1>
-<form>
+<form method="POST">
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <label  class="form-label">Subject</label>
+    <input type="email" name="subject" class="form-control" >
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label  class="form-label">Email</label>
+    <input type="email" name="email" class="form-control" >
   </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  <div class="mb-3">
+    <label  class="form-label">Body</label>
+    <textarea name="body" class="form-control" id ="" cols="30" rows="10"></textarea>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
