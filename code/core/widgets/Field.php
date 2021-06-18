d41d18a59a869bf23409efbdee16e89e4873cc11
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-06-18 11:52:23 -0400

    prueba

--- code/core/widgets/Field.php
+++ code/core/widgets/Field.php
@@ -6,6 +6,8 @@ use app\core\Model;
 class Field 
 {
     public const  TYPE_TEXT ='text';
+    public const TYPE_MAIL = 'email';
+    public const TYPE_PASSWORD = 'password';
 
     public Model $model;
     public string $attribute;
@@ -19,6 +21,30 @@ class Field
     }
     public function __toString()
     {
-        
+        return sprintf('
+        <div class="mb-3">
+            <label class="form-label">%s</label>
+        <input type="%s" name="%s" value="%s" class="form-control %s">
+        <div class="invlaid-feedback">
+            %s
+        </div>
+      </div>
+        ',
+        $this->attribute,
+        $this->type,
+        $this->attribute,
+        $this->model->{$this->attribute},
+        $this->model->hasError($this->attribute) ? 'is-invalid' : '',
+        $this->model->getFirstError($this->attribute)
+        );
+    }
+    public function passwordField(){
+        $this->type = self::TYPE_PASSWORD;
+        return $this;
+
+    }
+    public function mailField(){
+        $this->type = self::TYPE_MAIL;
+        return $this;
     }
 }
--- code/views/register.php
+++ code/views/register.php
@@ -1,39 +1,16 @@
 <h1>Register</h1>
 
-
-<?php $form = \app\core\widgets\Form::begin('','POST')  ?>
-<?= $form->field($model,'firstname') ?>
-<?= $form->field($model,'lasttname') ?>
-<?= $form->field($model,'email') ?>
-<?= $form->field($model,'password') ?>
-<?= $form->field($model,'confirmPassword') ?>
-<?= \app\core\widgets\Form::end('')  ?>
-
-
-<form method="POST">
-  <div class="mb-3">
-    <label  class="form-label">Firstname</label>
-    <input type="text" name="firstname" value = "<?= $model->firstname ?>"
-     class="form-control <?= $model->haError('firstname') ? 'is-invalid' : '' ?>" >
-     <div class="invalid-feedback">
-     <?= $model->getFirstError('firstname') ?>
-     </div>
-  </div>
-  <div class="mb-3">
-    <label  class="form-label">Lastname</label>
-    <input type="text" name="lastname" class="form-control" >
-  </div>
-  <div class="mb-3">
-    <label  class="form-label">Email</label>
-    <input type="text" name="email" class="form-control" >
-  </div>
-  <div class="mb-3">
-    <label  class="form-label">Password</label>
-    <input type="password" name="password" class="form-control" >
-  </div>
-  <div class="mb-3">
-    <label  class="form-label">Confirm Password</label>
-    <input type="password" name="confirmPassword" class="form-control" >
-  </div>
-  <button type="submit" class="btn btn-primary">save</button>
-</form>
+<?php $form = \app\core\widgets\Form::begin('', 'POST') ?>
+<div class="row">
+<div class="col">
+<?= $form->field($model, 'firstName') ?>
+</div>
+<div class="col">
+<?= $form->field($model, 'lastName') ?>
+</div>
+</div>
+<?= $form->field($model, 'email')->mailField() ?>
+<?= $form->field($model, 'password')->passwordField() ?>
+<?= $form->field($model, 'confirmPassword')->passwordField() ?>
+<button type="submit" class="btn btn-primary">Save</button>
+<?= \app\core\widgets\Form::end() ?>
