4fe620835feb671f3ec48b55649fc5f941884109
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-06-18 11:39:43 -0400

    prueba

--- code/core/Database.php
+++ code/core/Database.php
@@ -9,9 +9,9 @@ class Database
     public function __construct(array $config)
     {
         // .env
-        $dsn = $config['DSN'] ?? '';
-        $username = $config['USERNAME'] ?? '';
-        $password = $config['PASSWORD'] ?? '';
+        $dsn = $config['dsn'] ?? '';
+        $username = $config['user'] ?? '';
+        $password = $config['password'] ?? '';
 
         $this->pdo = new \PDO($dsn, $username, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
