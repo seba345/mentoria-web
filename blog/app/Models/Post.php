d6691e24ad1d07bd4b8bc99f7049f041ed78e0fd
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-09-03 16:00:25 -0400

    modificando blade

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -9,5 +9,6 @@ class Post extends Model
 {
     //Traits 
     use HasFactory;
-    public $fillable =['title','resumen','body']; 
+    //public $fillable =['title','resumen','body']; 
+    public $guarded = ['id'];
 }
