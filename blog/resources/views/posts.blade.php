a0a879a506d9b1af9c2c865636b31347c29c6974
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-09-07 15:48:40 -0300

    agregando migrate

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -4,7 +4,7 @@
     @foreach ($posts as $post)
         <article>   
         <h1>
-            <a href="/post/<?= $post->id ?>"> 
+            <a href="/post/<?= $post->slug ?>"> 
                 {{ $post->title }}
                 </a>
     </h1>    
