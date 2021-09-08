523542160142af5123a5242af50ca836cc50a2a9
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-09-08 12:38:07 -0300

    prueba

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -4,7 +4,7 @@
     @foreach ($posts as $post)
         <article>   
         <h1>
-            <a href="/posts/<?= $post->slug ?>"> 
+            <a href="/post/<?= $post->slug ?>"> 
                 {{ $post->title }}
                 </a> 
     </h1>    
