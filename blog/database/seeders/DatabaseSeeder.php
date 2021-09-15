2498f44b9f8cfb6e0bc3a07766b33a35d955f5f8
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-09-15 15:57:21 -0300

    se agrega nuevos seed

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Post;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,19 +15,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory()->create();
+        User::truncate();
+        Category::truncate();
 
-            Category::created([
+
+         $user = User::factory()->create();
+
+            $personal = Category::create([
                 'name' => 'Personal',
                 'slug' => 'personal'
             ]);
-            Category::created([
+            $work = Category::create([
                 'name' => 'Work',
                 'slug' => 'work'
             ]);
-            Category::created([
+            $hobbies = Category::create([
                 'name' => 'Hobbies',
                 'slug' => 'hobbies'
+            ]);
+        
+            Post::create([
+                'category_id' => $work->id,
+                'user_id' => $user->id,
+                'slug' => 'my-first-post',
+                'title' => 'My First Post',
+                'resumen' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when ',
+                'body' =>'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'
             ]);
 
     }
