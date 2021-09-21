41195948b2a4e17623f36024fb81c278d1f873ef
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-09-21 15:36:49 -0300

    generamos seed

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -17,9 +17,12 @@ class DatabaseSeeder extends Seeder
     {
         User::truncate();
         Category::truncate();
+        Post::truncate();
+
+        Post::factory()->create();
 
 
-         $user = User::factory()->create();
+       /*  $user = User::factory()->create();
 
             $personal = Category::create([
                 'name' => 'Personal',
@@ -43,7 +46,7 @@ class DatabaseSeeder extends Seeder
                 'title' => 'My First Post',
                 'resumen' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when ',
                 'body' =>'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'
-            ]);
+            ]);*/
  
     }
 }
