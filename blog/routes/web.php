601ffae74d64eb86f5628a4e8db22e8689f3745f
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-09-21 15:51:53 -0300

    generamos seed

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -39,7 +39,9 @@ Route::get('/', function () {
                 );*/
             
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest('published_at')
+        ->with('category')
+        ->get()
     ]);
 });
 
