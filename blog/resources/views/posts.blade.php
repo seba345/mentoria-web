5c7af174e5a7803166f638edf7bb560b645df2a0
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-09-29 15:31:20 -0300

    agregamos banner

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -1,5 +1,5 @@
 @extends('layout')
-@seccion('banner')
+@section('banner')
 <header class="max-w-xl mx-auto mt-20 text-center">
             <h1 class="text-4xl">
                 Latest <span class="text-blue-500">Laravel From Scratch</span> News
@@ -68,6 +68,7 @@
      @endsection
 
 @section('content')
+<div class="lg:grid lg:grid-cols-3">
     @foreach ($posts as $post)
         <article>   
         <h1>
