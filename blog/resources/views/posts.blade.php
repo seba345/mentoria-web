77dbf3869ea45f934627d192cef0bf7d3e1c2fdf
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-09-29 15:45:13 -0300

    agregamos banner

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -80,17 +80,18 @@
                         <div class="mt-8 flex flex-col justify-between">
                             <header>
                                 <div class="space-x-2">
-                                    <a href="#"
+                                    <a href="/category/{{$post->category->slug}}"
                                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                                       style="font-size: 10px">Techniques</a>
-                                    <a href="#"
-                                       class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
-                                       style="font-size: 10px">Updates</a>
+                                       style="font-size: 10px">{{ $post->category->name }}</a>
                                 </div>
 
                                 <div class="mt-4">
                                     <h1 class="text-3xl">
-                                        This is a big title and it will look great on two or even three lines. Wooohoo!
+                                    <h1>
+            <a href="/post/<?= $post->slug ?>"> 
+                {{ $post->title }}
+                </a> 
+    </h1>  
                                     </h1>
 
                                     <span class="mt-2 block text-gray-400 text-xs">
@@ -101,14 +102,10 @@
 
                             <div class="text-sm mt-4">
                                 <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                {{ $post->resumen }}
                                 </p>
 
-                                <p class="mt-4">
-                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                </p>
+
                             </div>
 
                             <footer class="flex justify-between items-center mt-8">
@@ -121,7 +118,7 @@
                                 </div>
 
                                 <div>
-                                    <a href="#"
+                                    <a href="/post/{{$post->slug}}"
                                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
                                     >Read More</a>
                                 </div>
