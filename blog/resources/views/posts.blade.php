82bc3af34cc371d73607911f68970d84f284a11c
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-09-29 15:46:35 -0300

    agregamos banner

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -112,7 +112,7 @@
                                 <div class="flex items-center text-sm">
                                     <img src="./images/lary-avatar.svg" alt="Lary avatar">
                                     <div class="ml-3">
-                                        <h5 class="font-bold">Lary Laracore</h5>
+                                        <h5 class="font-bold">{{ $post->author->name }}</h5>
                                         <h6>Mascot at SEGIC</h6>
                                     </div>
                                 </div>
