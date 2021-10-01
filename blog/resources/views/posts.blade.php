a228d304026e2e22f6145d8c704d6c79f6d12e6d
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-10-01 15:50:59 -0300

    agregamos componente para los post

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -1,3 +1,21 @@
+
 <x-layout >
-HOLA SEGIC ! ESTOY UTILIZANDO VARIABLE SLOT
+@include('_posts-header')
+
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+
+    <x-post-main :post="$posts[0]" />
+
+    <div class="lg:grid lg:grid-cols-2">
+    <x-post-card />
+    <x-post-card />
+    </div>
+
+    <div class="lg:grid lg:grid-cols-3">
+    <x-post-card />
+    <x-post-card />
+    <x-post-card />
+       
+    </div>
+</main>
 </x-layout>
