af4f3a4469a52df5e7c7d601c5c5601114efc5e6
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-10-22 19:06:16 -0300

    aplicando componentes

--- blog/resources/views/components/dropdown-item.blade.php
+++ blog/resources/views/components/dropdown-item.blade.php
@@ -1,15 +1,13 @@
 @props(['actived' => false])
 
 @php
-
-$classes ='block text-left px-3 text-sm leading-3 hover:bg-blue-500 hover:text-white'
+$classes ='block text-left px-3 text-sm leading-3 hover:bg-blue-500 hover:text-white';
 
 if ($actived){
     $classes .= ' bg-blue-500 text-white';
 }
 
 @endphp
-
 
 <a {{ $attributes(['class' => $classes]) }}>
                         {{ $slot }}
