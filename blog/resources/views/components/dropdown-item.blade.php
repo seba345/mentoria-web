927ad444785a698757784be14eaf9b7bcee66c6c
sebastian diaz <sebastian.diaz@segic.usach.cl>
2021-10-22 19:04:03 -0300

    aplicando componentes

--- blog/resources/views/components/dropdown-item.blade.php
+++ blog/resources/views/components/dropdown-item.blade.php
@@ -4,13 +4,13 @@
 
 $classes ='block text-left px-3 text-sm leading-3 hover:bg-blue-500 hover:text-white'
 
-if ($active){
+if ($actived){
     $classes .= ' bg-blue-500 text-white';
 }
 
 @endphp
 
 
-<a {{ $attributes(['class' => $classes])}}>
+<a {{ $attributes(['class' => $classes]) }}>
                         {{ $slot }}
-                    </a>
+ </a>
